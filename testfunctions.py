e78c754996b55740c7b7e80d2eb54d89527891b0
KluEvo <keene.ruiyang.lu@gmail.com>
2024-11-13 15:16:35 -0600

    code for generation functions, not done

--- testfunctions.py
+++ testfunctions.py
@@ -1,45 +1,96 @@
-from flask import Flask, request
+from flask import Flask, Response, request, jsonify
 from flask_cors import CORS
-from JustificationExtraction import generateReasons, extractReasons, generateSentence
+from storyBeats import get_user_input, identify_story_structure, generate_story_choices, find_options
 from namerecognition import extract_names
 import random
 import time
- 
+
+starting_state = ""
+ending_state = ""
+beats = []
+context = ""
+heldInfo = ""
+heldInfoOld = ""
+opts = ""
+
+
 app = Flask(__name__)
 cors = CORS(app) 
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 
-def parsePrompt(prompt):
-    return prompt
+@app.after_request
+def basic_authentication():
+    if request.method.lower() == 'options':
+        return Response()
+def add_header(response):
+    headers = {'Access-Control-Allow-Origin': '*',
+               'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+               'Access-Control-Allow-Headers': 'Content-Type'}
+    
+    if request.method.lower() == 'options':
+        return jsonify(headers), 200
+
 
 @app.route('/characters', methods=['POST'])
-def getChars():
-    # Get the login credentials from the request
-    data = request.get_json()
-    charInfo = data['charInfo']
-    promptInfo = data['promptInfo']
-    
-    
-    print(charInfo, promptInfo)
+def getUserPrompt():
+    global starting_state
+    global ending_state
+    global beats
+    global context
+    beats = []
+    context = ""
+    starting_state, ending_state = get_user_input()
+    # starting_state = "Jon and Amy are rivals"
+    # ending_state = "Jon and Amy start dating"
+    story_structure = identify_story_structure(starting_state, ending_state)
+    # print("Identified Story Structure:")
+    # print(story_structure)
+    return story_structure.split('\n')
 
-    target = parsePrompt(promptInfo)
+def handelText(txt):
+    global context
+    global heldInfo
+    global opts
 
-    resultant = target
-    agents = extract_names(charInfo) 
-    print(f"list of agents: {agents}")
-    info = generateReasons(resultant)
-    reasonsList = extractReasons(info)
-    print("generating the flashback...")
-    ind = random.randint(0, len(reasonsList))
-    reason = reasonsList[ind]
-    time.sleep(1.5)
-    sentence = generateSentence(reason, resultant, agents)
-    time.sleep(1.5)
-    
-    return [extract_names(charInfo), sentence]
+    output = ""
+    giveChoice = False
+    if part.strip():
+        part = part.strip()    
+        if part[0].lower() == '*' or part[0].lower() == '-' :
+            context += part + " "
+            output = part
+        elif context != "":
+            opts = generate_story_choices(context, heldInfo, ending_state)
+            output = opts
+            heldInfo = part
+            giveChoice = True
 
+    return {
+        "outputTxt": output,
+        "choices": giveChoice
+    }
+
+def handleChoices(choice):
+    global ending_state
+    global beats
+    global heldInfoOld
+    global opts
+
+    # print(opts)
+    options = find_options(opts)
+    # choice = input("How do you want this to play out? ")
+    if choice.lower() == 'a':
+        choice = options[0]
+    elif choice.lower() == 'b':
+        choice = options[1]
+    else:
+        print("not valid choice, defaulting to the first option.")
+        choice = options[0]
+    context = ""
+    beats.append(choice)
+    return choice
 
 if __name__ == '__main__':
     app.run(debug=True) 
